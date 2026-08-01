aa65467006e740eebe1e208cbeb4f00fa401d672
Wasif <ansari22wasif@gmail.com>
2026-08-01 21:16:11 +0530

    Responsive Dhikr Tabs

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.vduqql49ds"
+    "revision": "0.ej98tkk4q78"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
